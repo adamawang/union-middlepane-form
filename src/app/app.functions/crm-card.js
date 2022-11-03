59dfa73168a9ecf1691102d63c0d85b1d6b08fba
Adam Wang <awang@hubspot.com>
2022-11-03 16:30:32 -0400

    Initial cards commit

--- src/app/app.functions/crm-card.js
+++ src/app/app.functions/crm-card.js
@@ -6,7 +6,7 @@ exports.main = async (context, sendResponse) => {
     sendResponse({
       message: {
         type: 'SUCCESS',
-        body: `Request was successful. Deal created for ${product_name}`,
+        body: `Request processed for ${product_name}.`,
       },
     });
   }
@@ -15,34 +15,7 @@ exports.main = async (context, sendResponse) => {
     sections: [
       {
         type: 'text',
-        text: "To request expedited shipping, fill out the form below.",
-      },
-      {
-        type: 'form',
-        content: [
-          {
-            type: 'input',
-            name: 'product_name',
-            inputType: 'text',
-            label: 'Product name',
-            initialValue: '',
-          },
-          {
-            type: 'input',
-            name: 'ship_date',
-            inputType: 'text',
-            label: 'Ship by date',
-            initialValue: '',
-          },
-          {
-            type: 'button',
-            text: 'Submit request',
-            onClick: {
-              type: 'SUBMIT',
-              serverlessFunction: 'crm-card',
-            },
-          },
-        ],
+        text: "Sample card",
       },
     ],
   });
