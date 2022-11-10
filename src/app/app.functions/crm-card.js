f75ab81a0ebf535703199cd379604e677c5befed
Adam Wang <awang@hubspot.com>
2022-11-10 09:41:19 -0500

    Empty

--- src/app/app.functions/crm-card.js
+++ src/app/app.functions/crm-card.js
@@ -3,42 +3,7 @@ const hubspot = require('@hubspot/api-client');
 exports.main = async (context, sendResponse) => {
   const { event } = context;
 
-  if (event && event.type === 'SUBMIT') {
-
-    sendResponse({
-      message: {
-        type: 'SUCCESS',
-        body: `Successfully submitted!`,
-      },
-    });
-  }
-
   sendResponse({
-    sections: [
-      {
-        type: 'text',
-        text: "Form example",
-      },
-      {
-        type: 'form',
-        content: [
-          {
-            type: 'input',
-            name: 'name',
-            inputType: 'text',
-            label: 'Name',
-            initialValue: '',
-          },
-          {
-            type: 'button',
-            text: 'Submit request',
-            onClick: {
-              type: 'SUBMIT',
-              serverlessFunction: 'crm-card',
-            },
-          },
-        ],
-      },
-    ],
+    sections: [],
   });
 };
