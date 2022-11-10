db4d333e2de694b31e577e1d8296e92641681d6e
Adam Wang <awang@hubspot.com>
2022-11-10 09:20:23 -0500

    Revert commit

--- src/app/app.functions/crm-card.js
+++ src/app/app.functions/crm-card.js
@@ -3,68 +3,30 @@ const hubspot = require('@hubspot/api-client');
 exports.main = async (context, sendResponse) => {
   const { event } = context;
 
-  const { hs_ticket_id, shipping } = context.propertiesToSend;
-
-  const hs = new hubspot.Client({
-    accessToken: context.secrets.PRIVATE_APP_ACCESS_TOKEN
-  });
-
   if (event && event.type === 'SUBMIT') {
-    const { product_name, ship_date } = event.payload.formState;
 
-    const ticketObj = {
-      properties: {
-        product_name,
-        shipping: 'expedited',
-        ship_date,
+    sendResponse({
+      message: {
+        type: 'SUCCESS',
+        body: `Success!`,
       },
-    }
-
-    try {
-      await hs.crm.tickets.basicApi.update(hs_ticket_id, ticketObj);
-
-      sendResponse({
-        message: {
-          type: 'SUCCESS',
-          body: `Request submitted. Creating deal for ${product_name}.`,
-        },
-      });
-    } catch (error) {
-      console.log("API error: ", error.message)
-      sendResponse({
-        message: {
-          type: 'ERROR',
-          body: `Request failed for ${product_name}.`,
-        },
-      });
-    }
-
-
+    });
   }
 
   sendResponse({
     sections: [
       {
         type: 'text',
-        text: "To request expedited shipping, fill out the form below.",
+        text: "Form example",
       },
       {
         type: 'form',
         content: [
           {
             type: 'input',
-            name: 'product_name',
+            name: 'name',
             inputType: 'text',
-            label: 'Product name',
-            readonly: true,
-            initialValue: 'Credit card reader',
-          },
-          {
-            type: 'input',
-            name: 'ship_date',
-            inputType: 'text',
-            label: 'Ship by date',
-            readonly: shipping === 'expedited',
+            label: 'Name',
             initialValue: '',
           },
           {
