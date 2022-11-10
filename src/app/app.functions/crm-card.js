35d4cc0ce934602cb659eac173b8ca5ed1142f02
Adam Wang <awang@hubspot.com>
2022-11-10 10:05:40 -0500

    Update 34

--- src/app/app.functions/crm-card.js
+++ src/app/app.functions/crm-card.js
@@ -3,35 +3,72 @@ const hubspot = require('@hubspot/api-client');
 exports.main = async (context, sendResponse) => {
   const { event } = context;
 
-  if (event && event.type === 'SUBMIT') {
+  const { hs_ticket_id, shipping } = context.propertiesToSend;
 
-    sendResponse({
-      message: {
-        type: 'SUCCESS',
-        body: `Successfully submitted!`,
+  const hs = new hubspot.Client({
+    accessToken: context.secrets.PRIVATE_APP_ACCESS_TOKEN
+  });
+
+  if (event && event.type === 'SUBMIT') {
+    const { product_name, ship_date } = event.payload.formState;
+
+    const ticketObj = {
+      properties: {
+        product_name,
+        shipping: 'expedited',
+        ship_date,
       },
-    });
+    }
+
+    try {
+      await hs.crm.tickets.basicApi.update(hs_ticket_id, ticketObj);
+
+      sendResponse({
+        message: {
+          type: 'SUCCESS',
+          body: `Request submitted. Creating deal for ${product_name}.`,
+        },
+      });
+    } catch (error) {
+      console.log("API error: ", error.message)
+      sendResponse({
+        message: {
+          type: 'ERROR',
+          body: `Request failed for ${product_name}.`,
+        },
+      });
+    }
   }
 
   sendResponse({
     sections: [
       {
         type: 'text',
-        text: "Form example",
+        text: "To request expedited shipping, fill out the form below.",
       },
       {
         type: 'form',
         content: [
           {
             type: 'input',
-            name: 'name',
+            name: 'product_name',
             inputType: 'text',
-            label: 'Name',
+            label: 'Product name',
+            readonly: true,
+            initialValue: 'Credit card reader',
+          },
+          {
+            type: 'input',
+            name: 'ship_date',
+            inputType: 'text',
+            label: 'Ship by date',
+            readonly: shipping === 'expedited',
             initialValue: '',
           },
           {
             type: 'button',
             text: 'Submit request',
+            disabled: shipping === 'expedited',
             onClick: {
               type: 'SUBMIT',
               serverlessFunction: 'crm-card',
